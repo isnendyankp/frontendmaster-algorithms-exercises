9d4a7a4c27a6c55fdec2bb384b68eaefb68654f1
Isnendyan <Isnendyankp@gmail.com>
2025-03-06 15:13:01 +0700

    add solution code for insertion sort test

--- specs/insertion-sort/insertion-sort.test.js
+++ specs/insertion-sort/insertion-sort.test.js
@@ -14,12 +14,34 @@
 */
 
 function insertionSort(nums) {
-  // code goes here
+  // Mulai dari indeks 1 (elemen kedua) karena elemen pertama dianggap sudah terurut
+  for (let i = 1; i < nums.length; i++) {
+    // Simpan nilai yang akan kita sisipkan ke dalam variabel sementara
+    let insertNumber = nums[i]; // the insertNumber number we're looking to insert
+    
+    // Variabel j untuk mengecek dan menggeser elemen-elemen di bagian yang sudah terurut
+    let j; // the inner counter
+
+    // Loop untuk mencari posisi yang tepat di bagian yang sudah terurut
+    // nums[j] > insertNumber: jika elemen yang dicek lebih besar dari yang akan disisipkan
+    // j >= 0: pastikan tidak melewati batas kiri array
+    for (j = i - 1; nums[j] > insertNumber && j >= 0; j--) {
+      // Geser elemen yang lebih besar ke kanan satu posisi
+      // move numbers to the right until we find where to insert
+      nums[j + 1] = nums[j];
+    }
+
+    // Sisipkan nilai ke posisi yang tepat
+    // j + 1 karena j sudah dikurangi satu kali terakhir di loop sebelumnya
+    // do the insertion
+    nums[j + 1] = insertNumber;
+  }
+  return nums;
 }
 
 // unit tests
 // do not modify the below code
-test.skip("insertion sort", function () {
+test("insertion sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   insertionSort(nums);
   expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
